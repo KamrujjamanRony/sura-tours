61e8a0ed8bd67c188bd2a676794f37484fbf625c
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-06-09 01:37:02 +0600

    header done

---
+++ src/components/home/Header.js
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo-white.png';
+
+const Header = () => {
+    return (
+        <header class="header">
+            <div className='logo-box'>
+                <img src={logo} alt='logo' className='logo' />
+            </div>
+            <div className='text-box'>
+                <h1 className='heading-primary'>
+                    <span className='heading-primary-main'>Outdoors</span>
+                    <span className='heading-primary-sub'>is where life happens</span>
+                </h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
