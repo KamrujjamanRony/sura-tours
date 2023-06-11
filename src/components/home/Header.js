99fcb4e418e875323e8b9bc12f9325d77354dd74
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-06-11 12:50:06 +0600

    BEM method applied on header section

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -3,15 +3,15 @@ import logo from '../../assets/img/logo-white.png';
 const Header = () => {
     return (
         <header class="header">
-            <div className='logo-box'>
-                <img src={logo} alt='logo' className='logo' />
+            <div className='header__logo-box'>
+                <img src={logo} alt='logo' className='header__logo' />
             </div>
-            <div className='text-box'>
+            <div className='header__text-box'>
                 <h1 className='heading-primary'>
-                    <span className='heading-primary-main'>Outdoors</span>
-                    <span className='heading-primary-sub'>is where life happens</span>
+                    <span className='heading-primary--main'>Outdoors</span>
+                    <span className='heading-primary--sub'>is where life happens</span>
                 </h1>
-                <a href='#' className='btn btn-white btn-animation'>Discover our tours</a>
+                <a href='#' className='btn btn--white btn--animation'>Discover our tours</a>
             </div>
         </header>
     );
