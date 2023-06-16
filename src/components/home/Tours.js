94b92463cf0a9d06129fa4c2ff5a5db51493fb28
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-06-17 02:59:06 +0600

    tours section added

---
+++ src/components/home/Tours.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Tours = () => {
+  return (
+    <section className="section-tours">
+      <div className="u-center-text u-mb-big">
+        <h2 className="heading-secondary">Most popular tours</h2>
+      </div>
+      <div className="row">
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front">card no 01 front</div>
+            <div className="card__side card__side--back card__side--back-1">card no 01 back</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/home/Header";
 import About from "../components/home/About";
 import Feature from "../components/home/Feature";
+import Tours from "../components/home/Tours";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <main>
         <About />
         <Feature />
+        <Tours />
       </main>
     </>
   );
