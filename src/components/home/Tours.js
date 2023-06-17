bdff49b001a3a175a74b7ab5e0bb12b613d8e4bd
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-06-17 20:30:07 +0600

    tours card done

--- src/components/home/Tours.js
+++ src/components/home/Tours.js
@@ -9,8 +9,32 @@ const Tours = () => {
       <div className="row">
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">card no 01 front</div>
-            <div className="card__side card__side--back card__side--back-1">card no 01 back</div>
+            <div className="card__side card__side--front">
+              <div className="card__img card__img--1">&nbsp;</div>
+              <div className="card__heading">
+                <span className="card__heading-span card__heading-span--1">
+                  The sea explorer
+                </span>
+              </div>
+              <div className="card__details">
+                <ul>
+                  <li>2 day tours</li>
+                  <li>Up to 30 people</li>
+                  <li>2 tour guides</li>
+                  <li>Sleep in cozy hotels</li>
+                  <li>Difficulty: easy</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-1">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <div className="card__price-only">Only</div>
+                  <div className="card__price-value">$297</div>
+                </div>
+                <a href="#" className="btn btn--white">Book now</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
