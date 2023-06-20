7655ef0107d85e6bdc40ee0a6d0dfb73d81ef7a0
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-06-20 23:57:57 +0600

    modal design complete

--- src/components/shared/Popup.js
+++ src/components/shared/Popup.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import img1 from '../../assets/img/nat-8.jpg';
+import img2 from '../../assets/img/nat-9.jpg';
 
 const Popup = () => {
     return (
         <div className="popup">
             <div className="popup__content">
-                popup
+                <div className="popup__left">
+                    <img src={img1} alt="tour pic" className="popup__img" />
+                    <img src={img2} alt="tour pic" className="popup__img" />
+                </div>
+                <div className="popup__right">
+                    <h2 className="heading-secondary u-mb-small">Start booking now</h2>
+                    <h3 className="heading-tertiary u-mb-small">Important &ndash; Please read these terms before booking</h3>
+                    <p className="popup__text u-mb-medium">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem alias a, nisi nobis vel minus enim voluptatibus veritatis placeat excepturi hic, voluptate maiores rem dignissimos eius pariatur aliquid atque asperiores?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem alias a, nisi nobis vel minus enim voluptatibus veritatis placeat excepturi hic, voluptate maiores rem dignissimos eius pariatur aliquid atque asperiores?
+                    </p>
+                    <a href="#" className="btn btn--primary">Book now</a>
+                </div>
             </div>
         </div>
     );
